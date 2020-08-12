dfb5db368c730d5483f802b92a380f6b29ce679d
kentonambara <kentonambara@gmail.com>
2020-08-12 10:44:33 -0400

    Add icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 function Footer() {
-  return <footer>Created by Kento Nambara, © 2020</footer>
+  return (
+    <footer>
+      <a
+        href="https://github.com/knambara"
+        target="_blank"
+        rel="noreferrer"
+        style={{ color: "white" }}
+      >
+        <FontAwesomeIcon className="footer__icon" icon={faGithub} size="2x" />
+      </a>
+      Created by Kento Nambara, © 2020
+      <a
+        href="https://www.linkedin.com/in/kento-nambara"
+        target="_blank"
+        rel="noreferrer"
+        style={{ color: "white" }}
+      >
+        <FontAwesomeIcon className="footer__icon" icon={faLinkedin} size="2x" />
+      </a>
+    </footer>
+  )
 }
 
 export default Footer

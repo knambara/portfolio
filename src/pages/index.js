ef66f386dffe8cd4b28a036e2e245622629797e7
kentonambara <kentonambara@gmail.com>
2020-11-27 10:09:59 -0500

    Change title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react"
 import App from "../components/App"
-import Head from "../components/Head"
+import { Helmet } from "react-helmet"
 
 function Index() {
   return (
     <>
-      <Head />
+      <Helmet>
+        <title>Kento Nambara</title>
+        <meta
+          name="description"
+          content="Kento Nambara is a student in Brown University studying Computer Science. He specializes in building fullstack applications as well as in iOS development."
+        />
+      </Helmet>
       <App />
     </>
   )
